d6f2c57717f5b7e255c64dcbb69061e7e7ac628a
davfront <contact@david-pereira.net>
2023-06-13 19:21:00 +0200

    test animated light

--- src/viewer/rt_viewer_render_frame.c
+++ src/viewer/rt_viewer_render_frame.c
@@ -6,7 +6,7 @@
 /*   By: dapereir <dapereir@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 16:35:43 by dapereir          #+#    #+#             */
-/*   Updated: 2023/06/12 09:46:28 by dapereir         ###   ########.fr       */
+/*   Updated: 2023/06/13 19:15:23 by dapereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,18 @@
 
 int	rt_viewer_render_frame(t_data *data)
 {
+	t_light	*light;
+
+	light = data->lights;
+	
+	t_float	angle = 0.1;
+    t_float cosAngle = cos(angle);
+    t_float sinAngle = sin(angle);
+
+    light->pos.x = light->pos.x * cosAngle + light->pos.z * sinAngle;
+    light->pos.y = light->pos.y;
+    light->pos.z = -light->pos.x * sinAngle + light->pos.z * cosAngle;
+
 	data->img.img = mlx_new_image(data->mlx, WIN_WIDTH, WIN_HEIGHT);
 	data->img.addr = mlx_get_data_addr(data->img.img, &data->img.bpp, \
 		&data->img.len, &data->img.endian);
--- src/viewer/rt_viewer_start.c
+++ src/viewer/rt_viewer_start.c
@@ -6,7 +6,7 @@
 /*   By: dapereir <dapereir@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 21:03:41 by dapereir          #+#    #+#             */
-/*   Updated: 2023/06/12 10:03:08 by dapereir         ###   ########.fr       */
+/*   Updated: 2023/06/13 19:11:26 by dapereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	rt_viewer_start(t_data *data)
 	data->win = mlx_new_window(data->mlx, WIN_WIDTH, WIN_HEIGHT, data->title);
 	ft_printf("[%s] File opened\n", data->title);
 	rt_viewer_hooks(data);
-	// mlx_loop_hook(data->mlx, rt_viewer_render_frame, data);
-	rt_viewer_render_frame(data);
+	mlx_loop_hook(data->mlx, rt_viewer_render_frame, data);
+	// rt_viewer_render_frame(data);
 	mlx_loop(data->mlx);
 }
