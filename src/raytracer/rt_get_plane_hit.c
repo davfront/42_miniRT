aeb823d82ce24d76e6e6be5c1ed9a44d27986826
Agoutcho <alexltdd@gmail.com>
2023-06-15 00:06:03 +0200

    fix bug when the plane is between cam and light

--- src/raytracer/rt_get_plane_hit.c
+++ src/raytracer/rt_get_plane_hit.c
@@ -22,10 +22,10 @@ t_hit	rt_get_plane_hit(t_ray ray, t_obj *obj)
 
 	plane = obj->plane;
 	denum = vec3_dot(ray.dir, plane.normal);
+	hit = rt_hit_default();
 	if (!denum)
 		return (hit);
 	num = vec3_dot(vec3_subtract(plane.point, ray.pos), plane.normal);
-	hit = rt_hit_default();
 	if (!obj || obj->type != PLANE)
 		return (hit);
 	t = num / denum;
@@ -35,6 +35,6 @@ t_hit	rt_get_plane_hit(t_ray ray, t_obj *obj)
 	hit.dist = t;
 	hit.color = plane.color;
 	hit.pos = vec3_add(ray.pos, vec3_scale(ray.dir, hit.dist));
-	hit.normal = vec3_scale(plane.normal, 1 - 2 * (denum < 0));
+	hit.normal = vec3_scale(plane.normal, 1 - 2 * (denum > 0));
 	return (hit);
 }
