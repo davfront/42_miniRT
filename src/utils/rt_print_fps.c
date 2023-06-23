8539dc1116fd11ac6bca2a6590cfe4981a4ded19
atchougo <atchougo@student.42lyon.fr>
2023-06-23 14:58:32 +0200

    to avoid macro from debug.h in final version

--- src/utils/rt_print_fps.c
+++ src/utils/rt_print_fps.c
@@ -6,7 +6,7 @@
 /*   By: atchougo <atchougo@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 22:19:26 by atchougo          #+#    #+#             */
-/*   Updated: 2023/06/21 22:26:39 by atchougo         ###   ########.fr       */
+/*   Updated: 2023/06/23 14:57:06 by atchougo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	rt_print_fps(struct	timeval start_time, struct	timeval end_time)
 
 	delta_time = (end_time.tv_sec - start_time.tv_sec) + \
 				(end_time.tv_usec - start_time.tv_usec) / 1000000.0;
-	printf(YELLOW_P);
+	printf("\033[0;33m");
 	printf("fps : %f\n", 1 / delta_time);
-	printf("\033M\033[2K"RESET_P);
+	printf("\033M\033[2K\033[0m");
 }
