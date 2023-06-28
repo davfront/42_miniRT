9ae6e819ad4ca576ebc88a605d0bb3a53cc640c9
Agoutcho <alexltdd@gmail.com>
2023-06-28 02:28:34 +0200

    Tout le cylindre mais pas de lumière

--- src/raytracer/rt_get_cylinder_hit.c
+++ src/raytracer/rt_get_cylinder_hit.c
@@ -29,8 +29,6 @@ static	int	rt_cyl_plane_hit(t_ray ray, t_obj *obj, t_float t_max, t_hit *hit)
 	t = num / denum;
 	if (!isfinite(t) || t < T_MIN || t > t_max)
 		return (0);
-	if (!hit)
-		return (1);
 	*hit = rt_hit_default();
 	hit->obj = obj;
 	hit->dist = t;
@@ -55,37 +53,9 @@ static t_float	nearest_dist(t_hit a, t_hit b, int vala, int valb)
 		return (a.dist);
 }
 
-// static void	rt_get_top_and_bot(t_vec3 *top, t_vec3 *bot, t_obj *obj)
-// {
-// 	t_vec3	dis;
-
-// 	dis = vec3_scale(obj->cylinder.axis, obj->cylinder.height / 2);
-// 	*top = vec3_add(obj->cylinder.center, dis);
-// 	*bot = vec3_subtract(obj->cylinder.center, dis);
-// }
-
-// // TODO change this
-// static t_vec3	pre_compute_coef(t_vec3 v1, t_vec3 v2)
-// {
-// 	return (vec3_subtract(v1, vec3_scale(v2, vec3_dot(v1, v2))));
-// }
-
-// int	rt_get_cylinder_hit(t_ray ray, t_obj *obj, t_float t_max, t_hit *hit)
-// {
-// 	t_vec3	top, bot;
-// 	t_vec3	quad;
-// 	t_vec3	oc, dir, ocdir;
-
-// 	rt_get_top_and_bot(&top, &bot, obj);
-// 	oc = vec3_subtract(ray.pos, bot);
-// 	dir = pre_compute_coef(ray.dir, obj->cylinder.axis);
-// 	ocdir = pre_compute_coef(oc, obj->cylinder.axis);
-// 	quad = 
-// }
-
 int	rt_get_cylinder_hit(t_ray ray, t_obj *obj, t_float t_max, t_hit *hit)
 {
-	t_float	t, tp;
+	t_float	t1, t2, tp;
 	t_vec3	A, B;
 	t_vec3	AB, AO, AOxAB, VxAB;
 	t_float	ab2, a, b, c, d;
@@ -137,26 +107,51 @@ int	rt_get_cylinder_hit(t_ray ray, t_obj *obj, t_float t_max, t_hit *hit)
 	b = vec3_dot(VxAB, AOxAB) * 2;
 	c = vec3_dot(AOxAB, AOxAB) - (obj->cylinder.radius * obj->cylinder.radius * ab2);
 	d = b * b - 4 * a * c;
-	if (d < 0)
-		return (0);
-	t = (-b - sqrt(d)) / (2 * a);
-	if (!isfinite(t) || t < T_MIN || t > t_max)
-		return (0);
-	intersection = vec3_add(ray.pos, vec3_scale(ray.dir, t));
-	projection = vec3_add(A, vec3_scale(AB, vec3_dot(AB, vec3_subtract(intersection, A)) / ab2)); 
-	if ((vec3_length(vec3_subtract(projection, A)) +\
-	 vec3_length(vec3_subtract(projection, B))) > vec3_length(AB))
-	 	return (0);
+	t1 = INFINITY;
+	t2 = INFINITY;
+	if (d >= 0)
+	{
+		t1 = (-b - sqrt(d)) / (2 * a);
+		t2 = (-b + sqrt(d)) / (2 * a);
+		if (t1 > t2)
+			t1 = t2;
+		if (!isfinite(t1) || t1 < T_MIN || t1 > t_max)
+			return (0);
+		intersection = vec3_add(ray.pos, vec3_scale(ray.dir, t1));
+		projection = vec3_add(A, vec3_scale(AB, vec3_dot(AB, vec3_subtract(intersection, A)) / ab2)); 
+		if ((vec3_length(vec3_subtract(projection, A)) +\
+		vec3_length(vec3_subtract(projection, B))) > vec3_length(AB))
+			t1 = INFINITY;
+	}
 	if (!hit)
 		return (1);
-	if (tp < ((-b + sqrt(d)) / (2 * a)))
+	if (tp <= t1)
 		t_e = 1;
 	if (t_e)
 	{
+		// if (vala && vec3_length(vec3_subtract(hit_a.pos, A)) <= obj->cylinder.radius)
+		// 	return (rt_cyl_plane_hit(ray, &ob_pa, t_max, hit));
+		// else if (valb && vec3_length(vec3_subtract(hit_b.pos, B)) <= obj->cylinder.radius)
+		// 	return (rt_cyl_plane_hit(ray, &ob_pb, t_max, hit));
+		// return (1);
 		if (hit_a.dist > hit_b.dist)
+		{
 			rt_cyl_plane_hit(ray, &ob_pb, t_max, hit);
+			if (vec3_length(vec3_subtract(hit->pos, B)) > obj->cylinder.radius)
+			{
+				*hit = rt_hit_default();
+				return (0);
+			}
+		}
 		else 
+		{
 			rt_cyl_plane_hit(ray, &ob_pa, t_max, hit);
+			if (vec3_length(vec3_subtract(hit->pos, A)) > obj->cylinder.radius)
+			{
+				*hit = rt_hit_default();
+				return (0);
+			}
+		}
 		// if (!rt_cyl_plane_hit(ray, &ob_pa, t_max, hit))
 		// 	rt_cyl_plane_hit(ray, &ob_pb, t_max, hit);
 	}
@@ -164,7 +159,7 @@ int	rt_get_cylinder_hit(t_ray ray, t_obj *obj, t_float t_max, t_hit *hit)
 	{
 		*hit = rt_hit_default();
 		hit->obj = obj;
-		hit->dist = t;
+		hit->dist = t1;
 		hit->color = obj->cylinder.color;
 		hit->pos = vec3_add(ray.pos, vec3_scale(ray.dir, hit->dist));
 		hit->normal = vec3_subtract(intersection, projection);	
