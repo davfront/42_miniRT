48264765da554e52c20419ddd9f7137a00f18569
Agoutcho <alexltdd@gmail.com>
2023-07-17 10:17:16 +0200

    better organisation

--- src/raytracer/rt_get_cylinder_hit.c
+++ src/raytracer/rt_get_cylinder_hit.c
@@ -12,181 +12,92 @@
 
 #include "minirt.h"
 
-// static t_float	nearest_dist(t_hit a, t_hit b, int vala, int valb)
-// {
-// 	if (!vala && !valb)
-// 		return (INFINITY);
-// 	if (!vala)
-// 		return (b.dist);
-// 	if (!valb)
-// 		return (a.dist);
-// 	if (a.dist > b.dist)
-// 		return (b.dist);
-// 	else
-// 		return (a.dist);
-// }
+static t_float	rt_quadratic_calcul(t_ray ray, t_cylinder cyl, t_vec3 *quad_va)
+{
+	t_vec3	AO;
+	t_vec3	AOxAB;
+	t_vec3	VxAB;
+
+	// AO is vector from A to ray positi, on / origin
+	AO = vec3_subtract(ray.pos, cyl.bot);
+	// AOxAB cross product between AO and AB
+	AOxAB = vec3_cross(AO, cyl.vec_height);
+	// VxAB V is ray direction
+	VxAB = vec3_cross(ray.dir, cyl.vec_height);
+	// quadratic coef a, b, c
+	quad_va->x = vec3_dot(VxAB, VxAB);
+	quad_va->y = vec3_dot(VxAB, AOxAB) * 2;
+	quad_va->z = vec3_dot(AOxAB, AOxAB) \
+					- (cyl.radius * cyl.radius * cyl.vec_h_square);
+	// discriminent
+	return (quad_va->y * quad_va->y - 4 * quad_va->x * quad_va->z);
+}
+
+static int	rt_find_roots(t_vec3 q_val, t_float d, t_float t_max, t_float *t)
+{
+	t[0] = INFINITY;
+	t[1] = INFINITY;
+	t[0] = (-q_val.y - sqrt(d)) / (2 * q_val.x);
+	t[1] = (-q_val.y + sqrt(d)) / (2 * q_val.x);
+	if (t[0] > t[1])
+		t[0] = t[1];
+	if (!isfinite(t[0]) || t[0] < T_MIN || t[0] > t_max)
+		return (1);
+	return (0);
+}
+
+static void	rt_ip_calcul(t_ray ray, t_cylinder cyl, t_float *t, t_vec3 *ip)
+{
+	t_vec3	intersection;
+	t_vec3	projection;
+	t_float	proj_on_center;
+	t_vec3	hit_bot;
+
+	intersection = vec3_add(ray.pos, vec3_scale(ray.dir, t[0]));
+	ip[0] = intersection;
+	hit_bot = vec3_subtract(ip[0], cyl.bot);
+	proj_on_center = vec3_dot(cyl.vec_height, hit_bot) / cyl.vec_h_square;
+	projection = vec3_add(cyl.bot, vec3_scale(cyl.vec_height, proj_on_center));
+	ip[1] = projection;
+	ip[2] = vec3_subtract(intersection, projection);
+}
+
+static int	rt_is_in_cyl(t_cylinder cylinder, t_vec3 *inter_proj)
+{
+	t_float	top_vec_lenght;
+	t_float	bot_vec_lenght;
+
+	top_vec_lenght = vec3_length(vec3_subtract(inter_proj[1], cylinder.top));
+	bot_vec_lenght = vec3_length(vec3_subtract(inter_proj[1], cylinder.bot));
+	if ((bot_vec_lenght + top_vec_lenght) > vec3_length(cylinder.vec_height))
+		return (0);
+	return (1);
+}
 
 int	rt_get_cylinder_hit(t_ray ray, t_obj *obj, t_float t_max, t_hit *hit)
 {
-	t_float	t1, t2;
-	t_vec3	AO, AOxAB, VxAB;
-	t_float	a, b, c, d;
-	t_vec3	intersection, projection;
-	t_cylinder	cylinder;
+	t_float		t[2];
+	t_float		d;
+	t_vec3		quad_val;
+	t_vec3		in_proj[3];
+	t_cylinder	cyl;
 	
 	if (!obj || obj->type != CYLINDER)
 		return (0);
-	cylinder = obj->cylinder;
-	// AO is vector from A to ray position / origin
-	AO = vec3_subtract(ray.pos, cylinder.bot);
-	// AOxAB cross product between AO and AB
-	AOxAB = vec3_cross(AO, cylinder.vec_height);
-	// VxAB V is ray direction
-	VxAB = vec3_cross(ray.dir, cylinder.vec_height);
-	// quadratic coef a, b, c
-	a = vec3_dot(VxAB, VxAB);
-	b = vec3_dot(VxAB, AOxAB) * 2;
-	c = vec3_dot(AOxAB, AOxAB) - (obj->cylinder.radius * obj->cylinder.radius * cylinder.vec_h_square);
-	// discriminent
-	d = b * b - 4 * a * c;
-	t1 = INFINITY;
-	t2 = INFINITY;
-	// getting roots t1 and t2
-	if (d >= 0)
-	{
-		t1 = (-b - sqrt(d)) / (2 * a);
-		t2 = (-b + sqrt(d)) / (2 * a);
-		if (t1 > t2)
-			t1 = t2;
-		if (!isfinite(t1) || t1 < T_MIN || t1 > t_max)
-			return (0);
-		intersection = vec3_add(ray.pos, vec3_scale(ray.dir, t1));
-		projection = vec3_add(cylinder.bot, vec3_scale(cylinder.vec_height, vec3_dot(cylinder.vec_height, vec3_subtract(intersection, cylinder.bot)) / cylinder.vec_h_square)); 
-		if ((vec3_length(vec3_subtract(projection, cylinder.bot)) +\
-		vec3_length(vec3_subtract(projection, cylinder.top))) > vec3_length(cylinder.vec_height))
-			t1 = INFINITY;
-	}
+	cyl = obj->cylinder;
+	d = rt_quadratic_calcul(ray, cyl, &quad_val);
+	if (d < 0 || rt_find_roots(quad_val, d, t_max, t))
+		return (0);
+	rt_ip_calcul(ray, cyl, t, in_proj);
+	if (!rt_is_in_cyl(cyl, in_proj))
+		t[0] = INFINITY;
 	if (!hit)
 		return (1);
 	*hit = rt_hit_default();
 	hit->obj = obj;
-	hit->dist = t1;
-	hit->color = obj->cylinder.color;
+	hit->dist = t[0];
+	hit->color = cyl.color;
 	hit->pos = vec3_add(ray.pos, vec3_scale(ray.dir, hit->dist));
-	hit->normal = vec3_scale(vec3_subtract(intersection, projection), 1 / obj->cylinder.radius);	
+	hit->normal = vec3_scale(in_proj[2], 1 / cyl.radius);
 	return (1);
 }
-
-// int	rt_get_cylinder_hit(t_ray ray, t_obj *obj, t_float t_max, t_hit *hit)
-// {
-// 	t_float	t1, t2, tp;
-// 	t_vec3	A, B;
-// 	t_vec3	AB, AO, AOxAB, VxAB;
-// 	t_float	ab2, a, b, c, d;
-// 	t_vec3	intersection, projection;
-// 	t_vec3	displacement;
-// 	t_hit	hit_a;
-// 	t_hit	hit_b;
-// 	t_obj	ob_pa;
-// 	t_obj	ob_pb;
-// 	int		vala;
-// 	int		valb;
-// 	int		t_e = 0;
-	
-// 	// create top and bot plane
-// 	ob_pa.type = PLANE;
-// 	ob_pb.type = PLANE;
-// 	if (!obj || obj->type != CYLINDER)
-// 		return (0);
-// 	// distance from center to one side
-// 	displacement = vec3_scale(obj->cylinder.axis, obj->cylinder.height / 2);
-// 	// A is bottom point center
-// 	A = vec3_subtract(obj->cylinder.center, displacement);
-// 	ob_pa.plane.point = A;
-// 	ob_pa.plane.normal = obj->cylinder.axis;
-// 	ob_pa.plane.color = obj->cylinder.color;
-// 	// getting hit point of bottom plane in hit_a
-// 	vala = rt_get_plane_hit(ray, &ob_pa, t_max, &hit_a);
-// 	// B is top point center
-// 	B = vec3_add(obj->cylinder.center, displacement);
-// 	ob_pb.plane.point = B;
-// 	ob_pb.plane.normal = obj->cylinder.axis;
-// 	ob_pb.plane.color = obj->cylinder.color;
-// 	// getting hit point of bottom plane in hit_b
-// 	valb = rt_get_plane_hit(ray, &ob_pb, t_max, &hit_b);
-// 	// Check if the hitting point is inside the circle
-// 	if (valb && !(vec3_length(vec3_subtract(hit_a.pos, B)) <= obj->cylinder.radius))
-// 		valb = 0;
-// 	if (vala && !(vec3_length(vec3_subtract(hit_b.pos, A)) <= obj->cylinder.radius))
-// 		vala = 0;
-// 	// get the nearest hit point
-// 	tp = nearest_dist(hit_a, hit_b, vala, valb);
-// 	// AB is vector from bot to top
-// 	AB = vec3_subtract(B, A);
-// 	// AO is vector from A to ray position / origin
-// 	AO = vec3_subtract(ray.pos, A);
-// 	// AOxAB cross product between AO and AB
-// 	AOxAB = vec3_cross(AO, AB);
-// 	// VxAB V is ray direction
-// 	VxAB = vec3_cross(ray.dir, AB);
-// 	ab2 = vec3_dot(AB, AB);
-// 	// quadratic coef a, b, c
-// 	a = vec3_dot(VxAB, VxAB);
-// 	b = vec3_dot(VxAB, AOxAB) * 2;
-// 	c = vec3_dot(AOxAB, AOxAB) - (obj->cylinder.radius * obj->cylinder.radius * ab2);
-// 	// discriminent
-// 	d = b * b - 4 * a * c;
-// 	t1 = INFINITY;
-// 	t2 = INFINITY;
-// 	// getting roots t1 and t2
-// 	if (d >= 0)
-// 	{
-// 		t1 = (-b - sqrt(d)) / (2 * a);
-// 		t2 = (-b + sqrt(d)) / (2 * a);
-// 		if (t1 > t2)
-// 			t1 = t2;
-// 		if (!isfinite(t1) || t1 < T_MIN || t1 > t_max)
-// 			return (0);
-// 		intersection = vec3_add(ray.pos, vec3_scale(ray.dir, t1));
-// 		projection = vec3_add(A, vec3_scale(AB, vec3_dot(AB, vec3_subtract(intersection, A)) / ab2)); 
-// 		if ((vec3_length(vec3_subtract(projection, A)) +
-// 		vec3_length(vec3_subtract(projection, B))) > vec3_length(AB))
-// 			t1 = INFINITY;
-// 	}
-// 	if (!hit)
-// 		return (1);
-// 	// choosing between plane or cylinder
-// 	if (tp <= t1)
-// 		t_e = 1;
-// 	if (t_e)
-// 	{
-// 		if (hit_a.dist > hit_b.dist)
-// 		{
-// 			*hit = hit_b;
-// 			if (vec3_length(vec3_subtract(hit->pos, B)) > obj->cylinder.radius)
-// 			{
-// 				*hit = rt_hit_default();
-// 				return (0);
-// 			}
-// 		}
-// 		else
-// 		{
-// 			*hit = hit_a;
-// 			if (vec3_length(vec3_subtract(hit->pos, A)) > obj->cylinder.radius)
-// 			{
-// 				*hit = rt_hit_default();
-// 				return (0);
-// 			}
-// 		}
-// 	}
-// 	else
-// 	{
-// 		*hit = rt_hit_default();
-// 		hit->obj = obj;
-// 		hit->dist = t1;
-// 		hit->color = obj->cylinder.color;
-// 		hit->pos = vec3_add(ray.pos, vec3_scale(ray.dir, hit->dist));
-// 		hit->normal = vec3_scale(vec3_subtract(intersection, projection), 1 / obj->cylinder.radius);	
-// 	}
-// 	return (1);
-// }
